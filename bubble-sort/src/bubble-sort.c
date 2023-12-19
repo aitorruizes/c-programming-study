593faae771e7e4006039c624490d53bf79d7b4c1
Aitor Ruiz Agra <aragra.external@axa-assistance.us>
2023-12-19 17:25:00 -0300

    fix: adjust code indentation

--- bubble-sort/src/bubble-sort.c
+++ bubble-sort/src/bubble-sort.c
@@ -4,58 +4,58 @@
 
 void printConsoleHeader()
 {
-	printf("+-----------------------+\n");
-	printf("| BUBBLE SORT ALGORITHM |\n");
-	printf("+-----------------------|\n");
+  printf("+-----------------------+\n");
+  printf("| BUBBLE SORT ALGORITHM |\n");
+  printf("+-----------------------|\n");
 }
 
 void printFormattedArray(const char message[], int array[], const int arraySize)
 {
-	printf("%s\n", message);
+  printf("%s\n", message);
 
-	for (int i = 0; i < arraySize; i++)
-	{
-		if (i == 0)
-		{
-			printf("-> [");
-		}
+  for (int i = 0; i < arraySize; i++)
+  {
+    if (i == 0)
+    {
+      printf("-> [");
+    }
 
-		if (i == (arraySize - 1))
-		{
-			printf("%d]\n", array[i]);
-		}
-		else
-		{
-			printf("%d, ", array[i]);
-		}
-	}
+    if (i == (arraySize - 1))
+    {
+      printf("%d]\n", array[i]);
+    }
+    else
+    {
+      printf("%d, ", array[i]);
+    }
+  }
 }
 
 void bubbleSort(int array[], const int arraySize)
 {
-	bool hasSwapped;
+  bool hasSwapped;
 
-	do
-	{
-		hasSwapped = false;
-		
-		for (int i = 0; i < arraySize; i++)
-		{
-			if ((i + 1) > (arraySize - 1))
-			{
-				break;
-			}
+  do
+  {
+    hasSwapped = false;
+    
+    for (int i = 0; i < arraySize; i++)
+    {
+      if ((i + 1) > (arraySize - 1))
+      {
+        break;
+      }
 
-			if (array[i] > array[i + 1])
-			{
-				int temp = array[i];
+      if (array[i] > array[i + 1])
+      {
+        int temp = array[i];
 
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
 
-				hasSwapped = true;
-			}
-		}
+        hasSwapped = true;
+      }
+    }
 
-	} while (hasSwapped);
+  } while (hasSwapped);
 }
